75427eef38e45cbeeef1c62af621ad01026dc9f8
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2023-11-27 16:25:07 +0100

    Add project files.

---
+++ Blok3Les2/Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blok3Les2
+{
+    public class Auto
+    {
+        //maak de volgende publieke eigenschappen aan
+        //string Merk
+        //string Type
+        //string Kleur
+    }
+}
---
+++ Blok3Les2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Blok3Les2;
+
+List<Auto> Autos = new List<Auto>();
+
+Auto auto1 = new Auto();
+//vul de eigenschappen die je hebt aangemaakt in Auto.cs
+
+Autos.Add(auto1);
+
+Auto auto2 = new Auto();
+//vul de eigenschappen die je hebt aangemaakt in Auto.cs
+//Voeg deze ook toe aan de lijst Autos
+for (int i = 1; i<=5; i++ )
+{
+    //Maak binnen loop new Auto object aan
+    //Vraag aan gebruiker (Merk, Type en Kleur)
+    //Stel de eigenschappen in
+    //Voeg toe aan de lijst
+
+}
+
+
+foreach (Auto auto in Autos)
+{
+    //Druk het volgende af: "Het betreft een <Merk> van het type <Type> in de kleur <Kleur>
+    //Console.WriteLine("   ");
+}
